95ee60d30ccfac93fea2546f484b6e14c614f2bf
angular-cli <angular-cli@angular.io>
2017-01-29 20:05:34 -0500

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { EventViewCliPage } from './app.po';
+
+describe('event-view-cli App', function() {
+  let page: EventViewCliPage;
+
+  beforeEach(() => {
+    page = new EventViewCliPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
