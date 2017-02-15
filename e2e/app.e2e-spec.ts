8101b7aa615455f049dd12a1dc96cd46810e69e4
John Papa <john@johnpapa.net>
2017-02-14 20:40:36 -0500

    adjusted so the smoke e2e test runs and passes

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('event-view-cli App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Event View');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class EventViewCliPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('ev-app h1')).getText();
   }
 }
