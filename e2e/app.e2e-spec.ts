742b73d4e81315801d7f313f094cbdba084ef8f9
John Papa <john@johnpapa.net>
2017-02-14 22:41:10 -0500

    regex

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -13,40 +13,40 @@ describe('event-view-cli App', () => {
   describe('Dashboard page', () => {
     it('should display app title', () => {
       page.navigateToDashboard();
-      expect(page.getAppTitle()).toEqualText('event view');
+      expect(page.getAppTitle()).toMatch(/event view/i);
     });
 
     it('should display Han Solo as 4th button in dashboard', () => {
       page.navigateToDashboard();
-      expect(page.getDashboardButtonText(4)).toEqualText('han solo');
+      expect(page.getDashboardButtonText(4)).toMatch(/han solo/i);
     });
   });
 
   describe('Speakers page', () => {
     it('should display title', () => {
       page.navigateToSpeakers();
-      expect(page.getPageTitle()).toEqualText('speakers');
+      expect(page.getPageTitle()).toMatch(/speakers/i);
     });
   });
 
   describe('Sessions page', () => {
     it('should display title', () => {
       page.navigateToSessions();
-      expect(page.getPageTitle()).toEqualText('sessions');
+      expect(page.getPageTitle()).toMatch(/sessions/i);
     });
   });
 
   xdescribe('Admin page', () => {
     it('should display title', () => {
       page.navigateToAdmin();
-      expect(page.getPageTitle()).toEqualText('admin');
+      expect(page.getPageTitle()).toMatch(/admin/i);
     });
   });
 
   describe('Login page', () => {
     it('should display title', () => {
       page.navigateToLogin();
-      expect(page.getPageTitle()).toEqualText('login');
+      expect(page.getPageTitle()).toMatch(/login/i);
     });
   });
 
