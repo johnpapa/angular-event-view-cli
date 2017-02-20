90f334bd57f564b422e6fa66959eeaf275d7b1f9
John Papa <john@johnpapa.net>
2017-02-20 00:13:36 -0500

    wallaby is happy

--- wallaby.js
+++ wallaby.js
@@ -1,73 +1,43 @@
 var wallabyWebpack = require('wallaby-webpack');
 
 var webpackPostprocessor = wallabyWebpack({
-  entryPatterns: ['src/wallabyTest.js', 'src/**/*spec.js'],
+  entryPatterns: [
+    'src/wallaby-test.js',
+    'src/**/*.spec.js'
+  ],
 
   module: {
-    loaders: [{
-      test: /\.css$/,
-      loader: 'raw-loader'
-    }, {
-      test: /\.html$/,
-      loader: 'raw-loader'
-    }, {
-      test: /\.js$/,
-      loader: 'angular2-template-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.json$/,
-      loader: 'json-loader'
-    }, {
-      test: /\.styl$/,
-      loaders: ['raw-loader', 'stylus-loader']
-    }, {
-      test: /\.less$/,
-      loaders: ['raw-loader', 'less-loader']
-    }, {
-      test: /\.scss$|\.sass$/,
-      loaders: ['raw-loader', 'sass-loader']
-    }, {
-      test: /\.(jpg|png)$/,
-      loader: 'url-loader?limit=128000'
-    }]
+    loaders: [
+      { test: /\.css$/, loader: 'raw-loader' },
+      { test: /\.html$/, loader: 'raw-loader' },
+      { test: /\.js$/, loader: 'angular2-template-loader', exclude: /node_modules/ },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.styl$/, loaders: ['raw-loader', 'stylus-loader'] },
+      { test: /\.less$/, loaders: ['raw-loader', 'less-loader'] },
+      { test: /\.scss$|\.sass$/, loaders: ['raw-loader', 'sass-loader'] },
+      { test: /\.(jpg|png)$/, loader: 'url-loader?limit=128000' }
+    ]
   }
 });
 
-var compilerOptions = require('./src/tsconfig.json').compilerOptions;
+let compilerOptions = require('./src/tsconfig.json').compilerOptions;
 
 module.exports = function (wallaby) {
 
   return {
-    files: [{
-      pattern: 'src/**/*.ts',
-      load: false
-    }, {
-      pattern: 'src/**/*.d.ts',
-      ignore: true
-    }, {
-      pattern: 'src/**/*.css',
-      load: false
-    }, {
-      pattern: 'src/**/*.less',
-      load: false
-    }, {
-      pattern: 'src/**/*.scss',
-      load: false
-    }, {
-      pattern: 'src/**/*.sass',
-      load: false
-    }, {
-      pattern: 'src/**/*.html',
-      load: false
-    }, {
-      pattern: 'src/**/*spec.ts',
-      ignore: true
-    }],
+    files: [
+      { pattern: 'src/wallaby-test.js', load: false },
+      { pattern: 'src/**/*.ts', load: false },
+      { pattern: 'src/**/*.d.ts', ignore: true },
+      { pattern: 'src/**/*.css', load: false },
+      { pattern: 'src/**/*.less', load: false },
+      { pattern: 'src/**/*.scss', load: false },
+      { pattern: 'src/**/*.sass', load: false },
+      { pattern: 'src/**/*.html', load: false },
+      { pattern: 'src/**/*spec.ts', ignore: true }
+    ],
 
-    tests: [{
-      pattern: 'src/**/*spec.ts',
-      load: false
-    }],
+    tests: [{ pattern: 'src/**/*spec.ts', load: false }],
 
     testFramework: 'jasmine',
 
@@ -75,9 +45,7 @@ module.exports = function (wallaby) {
       '**/*.ts': wallaby.compilers.typeScript(compilerOptions)
     },
 
-    env: {
-      kind: 'electron'
-    },
+    env: { kind: 'electron' },
 
     postprocessor: webpackPostprocessor,
 
