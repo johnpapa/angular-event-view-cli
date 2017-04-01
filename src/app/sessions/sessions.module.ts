ea9fcb87c22b5e194df1f2e386f3697a44796219
John Papa <john@johnpapa.net>
2017-04-01 10:54:14 -0400

    comments only

--- src/app/sessions/sessions.module.ts
+++ src/app/sessions/sessions.module.ts
@@ -10,9 +10,9 @@ import { SessionService } from './shared/session.service';
   imports: [SessionsRoutingModule, SharedModule],
   declarations: [SessionButtonComponent, routedComponents],
 
-  // We can put this in the component or we can do it in the module.
-  // In the module, everyone gets it everywhere.
+  // We can put this service in the component or we can do it in the module.
+  // This module is lazy loaded, so providing a service here
+  // allows all features in this module to use it.
   providers: [SessionService]
 })
 export class SessionsModule { }
-// avoids having to lazy load with loadChildren: "app/sessions/session.module#SessionModule"
