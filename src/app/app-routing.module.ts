919c167211f9274f38eee367166d56182a8d750e
John Papa <john@johnpapa.net>
2019-07-16 12:18:07 -0400

    comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,48 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: OnDemandPreloadStrategy })],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      /**
+       * Preloading strategies:
+       *  - https://angular.io/guide/router#custom-preloading-strategy
+       *
+       * NoPreloading
+       *  - No bundles will preload
+       *  - built-in strategy
+       *
+       * PreloadAllModules
+       *  - All bundles will preload, automatically
+       *  - built-in strategy
+       *  - https://dev.to/angular/preload-all-angular-bundles-1b6l
+       *
+       * OptInPreloadStrategy
+       *  - set data.preload to true/false in the route configuration
+       *  - custom strategy
+       *  - https://dev.to/angular/you-pick-which-angular-bundles-to-preload-5l9
+       *
+       * NetworkAwarePreloadStrategy
+       *  - Customize which connections types to avoid
+       *    ['slow-2g', '2g', '3g', '4g' ]
+       *  - custom strategy
+       *  - https://dev.to/angular/preload-angular-bundles-when-good-network-connectivity-is-detected-j3a
+       *
+       * OnDemandPreloadStrategy
+       *  - Only preload when a specific event occurs.
+       *  - You control when it preloads and what preloads.
+       *    - Preload everything
+       *      this.preloadOnDemandService.startPreload('*');
+       *    - Preload a specific bundle
+       *      this.preloadOnDemandService.startPreload(routePath);
+       *  - custom strategy
+       *  - https://dev.to/angular/predictive-preloading-strategy-for-your-angular-bundles-4bgl
+       *
+       */
+
+      { preloadingStrategy: OnDemandPreloadStrategy }
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
