05ce56237d749169a5ac8d55a60a80c6ed7efb3d
John Papa <papa@Johns-MacBook-Pro.local>
2020-02-01 22:57:22 -0500

    logging interceptor changes

--- src/app/core/interceptors/log-response.interceptor.ts
+++ src/app/core/interceptors/log-response.interceptor.ts
@@ -6,19 +6,39 @@ import {
   HttpResponse
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, finalize } from 'rxjs/operators';
 
 export class LogResponseTimeInterceptor implements HttpInterceptor {
-  constructor() {}
+  /**
+   * Credit: https://angular.io/guide/http#http-interceptors
+   */
+  // intercept_alternative(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  //   const started = Date.now();
+  //   return next.handle(req).pipe(
+  //     tap(event => {
+  //       if (event instanceof HttpResponse) {
+  //         const elapsed = Date.now() - started;
+  //         console.log(`HTTP: Request for ${req.urlWithParams} took ${elapsed} ms.`);
+  //       }
+  //     })
+  //   );
+  // }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const started = Date.now();
+    let ok: string;
+
     return next.handle(req).pipe(
-      tap(event => {
-        if (event instanceof HttpResponse) {
-          const elapsed = Date.now() - started;
-          console.log(`HTTP: Request for ${req.urlWithParams} took ${elapsed} ms.`);
-        }
+      tap(
+        // Succeeds when there is a response; ignore other events
+        event => (ok = event instanceof HttpResponse ? 'succeeded' : ''),
+        // Operation failed; error is an HttpErrorResponse
+        error => (ok = 'failed')
+      ),
+      // Log when response observable either completes or errors
+      finalize(() => {
+        const elapsed = Date.now() - started;
+        console.log(`${req.method} "${req.urlWithParams}" \n\t ${ok} in ${elapsed} ms.`);
       })
     );
   }
